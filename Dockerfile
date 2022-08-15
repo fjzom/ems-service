9569d8dbf9896af882b63854f6aefb2438ebe694
Fxz8yv6 <FELIPE_ZACARIAS@homedepot.com>
2022-08-14 22:54:01 -0500

    first commit

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM java:8
-ADD build/libs/*.jar dockerapp.jar
-EXPOSE 8888
-ENTRYPOINT ["java", "-jar", "dockerapp.jar"]
-
-FROM postgres
-ENV POSTGRES_PASSWORD emsdb
-ENV POSTGRES_DB emsdb
-COPY src/main/resources/schema.sql /docker-entrypoint-initdb.d/
--- src/main/java/com/example/emsservice/repository/IEmployeeRepositoryCrud.java
+++
@@ -1,7 +0,0 @@
-package com.example.emsservice.repository;
-
-import com.example.emsservice.domain.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IEmployeeRepositoryCrud extends CrudRepository<Employee, String> {
-}
